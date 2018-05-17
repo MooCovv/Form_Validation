7c92ce7541db2ac9402eb8667932b3dfda8a8992
PHILIP ROBINSON <philip.robinson@lausd.net>
2018-05-16 19:13:38 -0700

    Finished!

--- Form_Validation/Form1.Designer.cs
+++ Form_Validation/Form1.Designer.cs
@@ -28,13 +28,205 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.firstNameBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lastNameBox = new System.Windows.Forms.TextBox();
+            this.addressBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.phoneBox = new System.Windows.Forms.TextBox();
+            this.zipBox = new System.Windows.Forms.TextBox();
+            this.stateBox = new System.Windows.Forms.TextBox();
+            this.cityBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // firstNameBox
+            // 
+            this.firstNameBox.Location = new System.Drawing.Point(13, 26);
+            this.firstNameBox.Name = "firstNameBox";
+            this.firstNameBox.Size = new System.Drawing.Size(178, 20);
+            this.firstNameBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last Name";
+            // 
+            // lastNameBox
+            // 
+            this.lastNameBox.Location = new System.Drawing.Point(12, 80);
+            this.lastNameBox.Name = "lastNameBox";
+            this.lastNameBox.Size = new System.Drawing.Size(179, 20);
+            this.lastNameBox.TabIndex = 2;
+            // 
+            // addressBox
+            // 
+            this.addressBox.Location = new System.Drawing.Point(12, 129);
+            this.addressBox.Name = "addressBox";
+            this.addressBox.Size = new System.Drawing.Size(179, 20);
+            this.addressBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Street Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "City";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Zip Code";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 313);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Phone";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 209);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "State";
+            // 
+            // phoneBox
+            // 
+            this.phoneBox.Location = new System.Drawing.Point(12, 329);
+            this.phoneBox.MaxLength = 10;
+            this.phoneBox.Name = "phoneBox";
+            this.phoneBox.Size = new System.Drawing.Size(127, 20);
+            this.phoneBox.TabIndex = 7;
+            // 
+            // zipBox
+            // 
+            this.zipBox.Location = new System.Drawing.Point(12, 277);
+            this.zipBox.MaxLength = 5;
+            this.zipBox.Name = "zipBox";
+            this.zipBox.Size = new System.Drawing.Size(100, 20);
+            this.zipBox.TabIndex = 6;
+            // 
+            // stateBox
+            // 
+            this.stateBox.Location = new System.Drawing.Point(12, 225);
+            this.stateBox.MaxLength = 2;
+            this.stateBox.Name = "stateBox";
+            this.stateBox.Size = new System.Drawing.Size(74, 20);
+            this.stateBox.TabIndex = 5;
+            // 
+            // cityBox
+            // 
+            this.cityBox.Location = new System.Drawing.Point(12, 178);
+            this.cityBox.Name = "cityBox";
+            this.cityBox.Size = new System.Drawing.Size(179, 20);
+            this.cityBox.TabIndex = 4;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(84, 378);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(222, 378);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 9;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(380, 418);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.cityBox);
+            this.Controls.Add(this.stateBox);
+            this.Controls.Add(this.zipBox);
+            this.Controls.Add(this.phoneBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.addressBox);
+            this.Controls.Add(this.lastNameBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.firstNameBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form Validation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox firstNameBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lastNameBox;
+        private System.Windows.Forms.TextBox addressBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox phoneBox;
+        private System.Windows.Forms.TextBox zipBox;
+        private System.Windows.Forms.TextBox stateBox;
+        private System.Windows.Forms.TextBox cityBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button exitButton;
     }
 }
 
--- Form_Validation/Form1.cs
+++ Form_Validation/Form1.cs
@@ -16,5 +16,112 @@ namespace Form_Validation
         {
             InitializeComponent();
         }
+
+        private bool LetterCheck (string input)
+        {
+            bool isValue = true;
+
+            foreach (char ch in input)
+            {
+                if (!char.IsLetter(ch))
+                {
+                    isValue = false;
+                }
+            }
+            return isValue;
+        }
+
+        private bool PunctuationCheck(string input)
+        {
+            bool isValue = true;
+
+            foreach (char ch in input)
+            {
+                if (char.IsPunctuation(ch))
+                {
+                    isValue = false;
+                }
+            }
+            return isValue;
+        }
+
+        private bool DigitCheck(string input)
+        {
+            bool isValue = true;
+
+            foreach (char ch in input)
+            {
+                if (!char.IsDigit(ch))
+                {
+                    isValue = false;
+                }
+            }
+            return isValue;
+        }
+
+        private int LengthCheck(string input)
+        {
+            int isValue = 0;
+
+            foreach (char ch in input)
+            {
+                if (char.IsDigit(ch))
+                {
+                    isValue++;
+                }
+            }
+            return isValue;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string firstName = firstNameBox.Text;
+            string lastName = lastNameBox.Text;
+            string streetAdress = addressBox.Text;
+            string city = cityBox.Text;
+            string state = stateBox.Text;
+            string zipCode = zipBox.Text;
+            string phone = phoneBox.Text;
+
+            if (!LetterCheck(firstName))
+            {
+                MessageBox.Show("First name invalid.");
+            }
+
+            if(!LetterCheck(lastName))
+            {
+                MessageBox.Show("last name invalid.");
+            }
+
+            if(!PunctuationCheck(streetAdress))
+            {
+                MessageBox.Show("Street address invalid.");
+            }
+
+            if(!LetterCheck(city))
+            {
+                MessageBox.Show("City invalid.");
+            }
+
+            if(!LetterCheck(state) || city.Length != 2)
+            {
+                MessageBox.Show("State invalid.");
+            }
+
+            if(!DigitCheck(zipCode) || zipCode.Length != 5)
+            {
+                MessageBox.Show("Zip Code invalid.");
+            }
+
+            if(!DigitCheck(phone) || phone.Length != 10)
+            {
+                MessageBox.Show("Phone number invalid.");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
